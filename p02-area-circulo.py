380da4f808333a9fd22b5df67803687a3856fe23
Estefanía CA <estefaniacda26@gmail.com>
2024-08-21 12:55:01 -0600

    Actividad 1

---
+++ p02-area-circulo.py
@@ -0,0 +1,10 @@
+#p02-area-ciculo - Calcula el area de un circulo
+
+
+import math
+
+print("Calculando el area de un circulo:\n")
+print("Dame el radio: ")
+radio = float(input())
+area = math.pi * radio ** 2
+print(f"El circulo de radio {radio} tiene un area de {area}")
---
+++ p03-area-triangulo.py
@@ -0,0 +1,8 @@
+# p03-area- triangulo - Calcular el area de un triangulo
+
+print("Calculando el area de un triangulo:\n")
+print("Dame la base y la altura separados por un Enter")
+      
+base, altura = int(input()), int(input())
+area = base * altura / 2
+print(f"El triangulo de base {base} y altura {altura} tiene un area de {area}")
