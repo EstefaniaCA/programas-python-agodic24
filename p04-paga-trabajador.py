e0e65fdf14128e67596fc6f142544e7802c9f4cf
Estefanía CA <estefaniacda26@gmail.com>
2024-08-21 13:08:24 -0600

    Actividad 2

--- p04-paga-trabajador.py
+++ p04-paga-trabajador.py
@@ -1,19 +1,17 @@
 # p04-paga-trabajador - Calcula la paga total de un trabajador
 
-print("Calculando la paga de un trabajador")
+print("Calculando la paga de un trabajador : \n")
 
-print("Nombre : ") 
-nombre = input()
-print("Horas trabajadas : ") 
-horas = int(input())
-print("Paga por hora : ") 
-paga = float(input())
-tasa = 0.3
+nombre = imput("Dame tu nombre ?")
+horas = int(imput("Horas ?"))
+paga = float(imput("Paga x hora ?"))
+tasa = 0.03
+
 pagabruta = horas * paga
 impuesto = pagabruta * tasa
 paganeta = pagabruta - impuesto
-print("Resumen de pagos:\n")
-print(f"El trabajador {nombre}, trabajo {horas} horas, con una paga de {paga} pesos por hora, impuesto de {tasa}%")
-print(f"Paga bruta {pagabruta}")
-print(f"Impuessto {impuesto}")
-print(f"Paga neta {paganeta}")
+
+print(f"el trabajador {nombre}, trabajo {horas} horas a una paga de  {paga} pesos, con una tasa de {tasa}")
+print(f"Paga Bruta : {pagabruta:,.2f}")
+print(f"Impuesto : {impuesto:,.2f}")
+print(f"Paga Neta : {paganeta:,.2f}")
