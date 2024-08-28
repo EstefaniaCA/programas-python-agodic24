f2778de2650caa2e44a75ef3a735973ad84f48e7
Estefanía CA <estefaniacda26@gmail.com>
2024-08-28 14:41:53 -0600

    Actividad 4

--- p17-convertir-temperatura.py
+++ p17-convertir-temperatura.py
@@ -1,21 +1,15 @@
 #Convertir temperaturas de celcius a farenheit y viceversa
 
-import os; os.system("clear")
+print("Conversión de temperaturas de centigrados a Fahrenheit \n")
+opcion = str.upper( input('Convertir a [C]entigrados o convertir a [F]ahrenheit') )
 
-print("Convertir tenperaturas de celcius a farenheit y viceversa\n") 
-
-print("[1] Convertir Farenheit a Celcius ") 
-print("[ 2 ] Convertir Celius a Farenheit ") 
-op=int(input("Elige?"))
-if op == 1:
-  print("Convirtiendo de Farenheit a Celcius")
-  temp = float(input("Dame los grados Farenheit ?")) 
-  res = (temp 32) * 5/9
-  print (f" (temp) Farenheit equivale a {res) Celcius") 
-
-else:
-
-  print("Convirtiendo de Celcius a Farenheit")
-  temp = float(input("Dame los grados Celcius ? ")) 
-  res = temp = 9/5 + 32
-  print(f" (temp) Celcius equivale a (res) Farenheit")
+if opcion == 'C':
+    print("\nConvirtiendo a Centigrados\n")
+    t = float(input("Ingresa los grados Fahrenheit: "))
+    r = (t - 32) * 5 / 9
+    print(f'{t}° Fahrenheit, equivalen a {r:.2f}° Centigrados')
+else :
+    print("\nConviritiendo a Fahrenheit\n")
+    t = float(input("Ingresa los grados Centigrados"))
+    r = ( t * 9 / 5 ) + 32
+    print(f"{t}° Fahrenheit, equivalen a {r:.2f}° Centigrados")
