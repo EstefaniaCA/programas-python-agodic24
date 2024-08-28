dfb8d4439709f3c17501184c67c989230206d6e6
Estefanía CA <estefaniacda26@gmail.com>
2024-08-28 14:41:38 -0600

    Actividad 4

---
+++ p15-verificar-numero.py
@@ -0,0 +1,14 @@
+# Verificar si un numero entero es positivo, negativo, o cero
+
+print("Verificar si un numero entero es positivo, negativo o es cero:\n")
+
+numero = int(input("Dame un numero entero ?" ))
+
+if numero > 0:
+ print("El numero es positivo")
+
+if numero < 0 :
+ print("El numero es negativo")
+
+if numero == 0 :
+ print("El numero es cero") 
