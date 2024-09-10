a37641aa0de3052c02e069b6dbe8c838df1e09bf
Estefanía CA <estefaniacda26@gmail.com>
2024-09-10 11:34:33 -0600

    Actividad 7

---
+++ p34-tabla-conversion.py
@@ -0,0 +1,31 @@
+#Imprimir una tabla de conversion
+
+import os
+
+os.system("")
+
+
+# Tabla de conversion de Peso a Dollar
+
+import os
+
+while(True):
+ os.system('clear')
+
+ tc = 20.71
+ pi = float(input("Valor inicial: "))
+ pf = float(input("Valor final : "))
+ c = pi
+
+ print("\nPesos\tDollar")
+ print("-"*15)
+
+ while c<=pf :
+  print(f'{c}\t{c/tc:.2f}')
+  c+=1
+ print("-"*15)
+
+ res=input('Deseas Continuar(S/N)? ')
+
+ if res.upper()=='N':
+  break
---
+++ p36-tabla-multiplicar.py
@@ -0,0 +1,16 @@
+# imprime la tabla de multiplicar 
+
+while(True):
+
+    t = int(input("De cuál número quieres imprimir su tabla de multiplicar?: "))
+    n = int(input("Hasta cuál número quieres imprimir?:  "))
+    c = 1
+
+    while( c <= n):
+        print(f'{t} x {c} = {t*c}')
+        c+=1
+
+    r=input("Deseas Continuar [S]/[N]? ")
+    if r.upper()=="N":
+        print("Saliendo...")
+        break
