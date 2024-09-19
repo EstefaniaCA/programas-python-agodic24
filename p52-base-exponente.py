4ac2e0d66b1bf7537570c1103ac79bdc80041b3c
Estefanía CA <estefaniacda26@gmail.com>
2024-09-19 13:47:59 -0600

    Actividad 8

--- p52-base-exponente.py
+++ p52-base-exponente.py
@@ -15,9 +15,11 @@ print(f"\nLa base {base} elevado a la {exponente} es {p}")
 
 
 
+
 p = 1
 for _ in range (exponente):
     p = p * base
 print(f"\nLa base {base} elevado a la {exponente} es {p}")
+
 
 
